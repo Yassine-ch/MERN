45a8998a9348678d7b07ff69053fd35e1fe659f7
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-06-21 18:02:00 +0100

    super ninja

---
+++ WEEK 1/DAY 3/Core/First JSX/first-jsx/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+        
+<div class="m-center">
+<h1> Hello Dojo !!!</h1>
+          <h3>Things i should to do : </h3>
+
+         <ul>
+            <li>Learn React</li>
+            <li>Climb Mt Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+          </ul>
+</div>
+
+      
+    </div>
+  );
+}
+
+export default App;
