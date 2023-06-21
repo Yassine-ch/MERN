a925b754f733fbc14c6389bc3f9a2fa2f4e23e09
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-06-21 18:31:59 +0100

    first-jsx

--- WEEK 1/DAY 3/Core/First JSX/first-jsx/src/App.js
+++ WEEK 1/DAY 3/Core/First JSX/first-jsx/src/App.js
@@ -10,12 +10,14 @@ function App() {
 <h1> Hello Dojo !!!</h1>
           <h3>Things i should to do : </h3>
 
-         <ul>
-            <li>Learn React</li>
+        
+          
+           <li>Learn React</li>
             <li>Climb Mt Everest</li>
             <li>Run a marathon</li>
             <li>Feed the dogs</li>
-          </ul>
+
+        
 </div>
 
       
