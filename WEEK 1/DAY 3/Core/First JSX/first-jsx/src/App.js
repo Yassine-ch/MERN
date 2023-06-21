c7871e93fd63a90e2c9903495884f5f7c6aa47ed
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-06-21 18:34:38 +0100

    first-JSX

--- WEEK 1/DAY 3/Core/First JSX/first-jsx/src/App.js
+++ WEEK 1/DAY 3/Core/First JSX/first-jsx/src/App.js
@@ -3,25 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
     
         
-<div class="m-center">
-<h1> Hello Dojo !!!</h1>
-          <h3>Things i should to do : </h3>
-
-        
-          
-           <li>Learn React</li>
-            <li>Climb Mt Everest</li>
-            <li>Run a marathon</li>
-            <li>Feed the dogs</li>
-
-        
-</div>
-
-      
-    </div>
+    <div className="App">
+            {/* jsx syntax to return from the App component */}
+            <h1>Hello Dojo!</h1>
+            <h2>Things I need to do:</h2>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </div>
   );
 }
 
