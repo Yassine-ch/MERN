50a64292c7c569d1391e37607529f4dd612bf34b
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-07-01 00:04:24 +0100

    axios

---
+++ WEEK 2/DAY1/Core/Axios Pokemon API/axios-api/src/App.js
@@ -0,0 +1,29 @@
+
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+    const [pokemonList, setPokemonList] = useState([]);
+
+    const getPokemon = () => {
+        axios
+            .get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+            .then((response) => setPokemonList(response.data.results));
+    };
+
+  return (
+    <div className="App">
+    <button onClick={() => getPokemon()}>Fetch Pokemon</button>
+    <ul>
+        {pokemonList.map((pokemon, index) => (
+            <li key={index} className="list-element">
+                {pokemon.name}
+            </li>
+        ))}
+    </ul>
+</div>
+);
+}
+
+export default App;
